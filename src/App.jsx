467f0d193a980e713bd2fe69e09adda936f2b87e
nguyenvanthuong23062006 <nguyenvanthuong23062006@gmail.com>
2026-03-14 17:22:55 +0700

    tich hon boostrap 5 va theem header vao

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ProductList from './ProductList'
+import Header from './Head'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header>
+
+    </Header>
       <ProductList>
         
       </ProductList>
